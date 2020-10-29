c5a42caffb442e247251efd505ff7784690e03ea
Younouss NIANG <mdev.nyssix@gmail.com>
2020-10-29 23:58:56 +0100

    refactoring part 2

--- src/main/java/model/HotDrink.java
+++ src/main/java/model/HotDrink.java
@@ -2,7 +2,7 @@ package model;
 
 public class HotDrink extends Drink{
 
-    public HotDrink(DrinkType type, SugarNumber sugarNumber, Boolean isExtraHot){
+    public HotDrink(DrinkType type, SugarNumber sugarNumber, boolean isExtraHot){
         super(type, sugarNumber,isExtraHot);
     }
 
