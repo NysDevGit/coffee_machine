151982051cb15c6e3d002a7944a7d3fb70539297
NysDevGit <36926581+NysDevGit@users.noreply.github.com>
2020-11-04 00:13:05 +0100

    First iteration 2 (#9)
    * First-iteration : Making drinks
    
    * refactoring
    
    Co-authored-by: Younouss NIANG <mdev.nyssix@gmail.com>

--- src/main/java/business/OrderSenderTranslator.java
+++ src/main/java/business/OrderSenderTranslator.java
@@ -6,11 +6,11 @@ import java.util.StringJoiner;
 
 public class OrderSenderTranslator {
 
-    public static String translateOrder(Order drinkOrder){
+  public static String translateOrder(Order order){
         StringJoiner stringOrder = new StringJoiner(":");
 
-        stringOrder.add(drinkOrder.getTypeCode());
-        stringOrder.add(drinkOrder.getSugarQuantity());
+        stringOrder.add(order.getTypeCode());
+        stringOrder.add(order.getSugarQuantity());
 
         return  stringOrder.toString();
     }
